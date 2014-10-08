59b539c1427803005c29f4bb3d6f53bbac170c49
supreme.testing.rus@gmail.com <supreme.testing.rus@gmail.com@a67e4f36-4e9d-a895-4f60-b2dded599e4f>
2014-10-08 21:13:34 +0000

    Переименовал пакет

---
+++ trunk/epamTask1/src/ru/epam/task1/string/MaxAndMinLengthString.java
@@ -0,0 +1,18 @@
+package ru.epam.task1.string;
+
+import ru.epam.task1.gui.Task;
+
+public class MaxAndMinLengthString extends Task {
+	
+	public MaxAndMinLengthString(String shortName) {
+		super(shortName);
+	}
+
+	@Override
+	public void drawTask() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ trunk/epamTask1/src/ru/epam/task1/string/SortString.java
@@ -0,0 +1,17 @@
+package ru.epam.task1.string;
+
+import ru.epam.task1.gui.Task;
+
+public class SortString extends Task {
+	
+	public SortString(String shortName) {
+		super(shortName);
+	}
+
+	@Override
+	public void drawTask() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
