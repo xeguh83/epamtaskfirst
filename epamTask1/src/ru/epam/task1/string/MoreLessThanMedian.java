ea565c774f9756115be688b50c253f0911b57eff
supreme.testing.rus <supreme.testing.rus@gmail.com>
2014-10-08 21:13:34 +0000

    Переименовал пакет

---
+++ epamTask1/src/ru/epam/task1/string/MoreLessThanMedian.java
@@ -0,0 +1,17 @@
+package ru.epam.task1.string;
+
+import ru.epam.task1.gui.Task;
+
+public class MoreLessThanMedian extends Task {
+	
+	public MoreLessThanMedian(String shortName) {
+		super(shortName);
+	}
+
+	@Override
+	public void drawTask() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
