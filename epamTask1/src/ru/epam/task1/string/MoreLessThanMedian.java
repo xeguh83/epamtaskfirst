c5528caca494347c13951cf3d44dcd8b0172451c
supreme.testing.rus <supreme.testing.rus@gmail.com>
2014-10-11 22:09:41 +0000

    Доделал 3е задание

--- epamTask1/src/ru/epam/task1/string/MoreLessThanMedian.java
+++ epamTask1/src/ru/epam/task1/string/MoreLessThanMedian.java
@@ -1,17 +1,61 @@
 package ru.epam.task1.string;
 
+import java.util.Scanner;
+
 import ru.epam.task1.gui.Task;
 
 public class MoreLessThanMedian extends Task {
+	private String incomingString = "Мама мыла раму. Мама. Мыла. Раму. Мама мыла раму вчера. Вчера.Мама будет мыть раму завтра";
 	
 	public MoreLessThanMedian(String shortName) {
 		super(shortName);
 	}
 
 	@Override
-	public void drawTask() {
+	protected void doLogic() {
 		// TODO Auto-generated method stub
-		
+		String[] strings = incomingString.split("\\.");
+		int mediana = 0;
+		for (String string : strings) {
+			mediana += string.length();
+		}
+		mediana = (int) (mediana / strings.length);
+		printResult(strings, mediana);
+	}
+
+	private void printResult(String[] strings, int mediana) {
+		// TODO Auto-generated method stub
+		drawTitle();
+		System.out.println("Строки с длинами меньше или равными средней (" + mediana + "):");
+		for (String string : strings) {
+			if (string.length() <= mediana) System.out.println(string + "("+ string.length() + ")");
+		}
+		System.out.println("Строки с длинами больше средней (" + mediana + "):");
+		for (String string : strings) {
+			if (string.length() > mediana) System.out.println(string + "("+ string.length() + ")");
+		}
+		System.out.print("Нажмите Enter для выхода в меню");
+		try {
+			Scanner in = new Scanner(System.in);
+			in.nextLine();
+		} catch (Exception e) {
+			// TODO: handle exception
+		System.out.println(e);
+		}
+
+	}
+
+	@Override
+	protected void drawTitle() {
+		// TODO Auto-generated method stub
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+							" ║     Задача №3. Ввести n строк с консоли. Вывести на консоль те строки,     ║\r\n" +
+							" ║             длина которых меньше (больше) средней, а также длину.          ║\r\n" +
+							" ╚════════════════════════════════════════════════════════════════════════════╝");
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+							" ║  Строки разделённые знаком \".\" буду считаны из файла data.properties из    ║\r\n" +
+							" ║              параметра task3. Нажмите Enter для загрузки данных.           ║\r\n" +
+							" ╚════════════════════════════════════════════════════════════════════════════╝");	
 	}
 
 }
