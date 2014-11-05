188feb75cd4a2aecb6fcb6675a125431ee0f3d2a
supreme.testing.rus <supreme.testing.rus@gmail.com>
2014-11-05 21:08:50 +0000

    12/23 без 5

--- epamTask2/src/ru/epam/task2/list/ArrayListVersusLinkedList.java
+++ epamTask2/src/ru/epam/task2/list/ArrayListVersusLinkedList.java
@@ -2,6 +2,8 @@ package ru.epam.task2.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 import ru.epam.task2.gui.Task;
 
@@ -16,6 +18,35 @@ public class ArrayListVersusLinkedList extends Task {
 	@Override
 	protected void doLogic() {
 		int arraySize = askArraySize(); 
+		Integer[] array = fillArrayOfInt(arraySize);
+		String option = askTypeOfCollection();
+		Integer answer = null;
+		if (option.equals("ArrayList")) {
+			answer = getAnswerUsingArrayList(array);
+		} else {
+			answer = getAnswerUsingLinkedList(array);
+		}
+		drawTitle();
+		System.out.println(" Оставшийся единственный элемент: " + answer);
+		printEmptyLines(14);
+	}
+
+	private Integer getAnswerUsingLinkedList(Integer[] array) {
+		LinkedList<Integer> list = new LinkedList<Integer>(Arrays.asList(array));
+		int index = 1;
+		while (list.size() > 1) {
+			if (list.size() < index) {
+				index = 1;
+			} else if (list.size() == index) {
+				index = 0;
+			} else {
+				list.remove(index);
+				index++;
+			}
+		}
+		return list.get(0);	}
+
+	private Integer getAnswerUsingArrayList(Integer[] array) {
 		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(array)); 
 		int index = 1;
 		while (list.size() > 1) {
@@ -28,26 +59,67 @@ public class ArrayListVersusLinkedList extends Task {
 				index++;
 			}
 		}
-		System.out.println(list.get(0));
+		return list.get(0);
 	}
 
+	private String askTypeOfCollection() {
+		while (true) {
+			drawTitle();
+			System.out.println(" Введите номер коллекции для обработки чилел: \r\n"
+					+ " 1) ArrayList \r\n"
+					+ " 2) LinkedList");
+			printEmptyLines(12);
+			int option;
+			try {
+				Scanner in = new Scanner(System.in);
+				option = Integer.parseInt(in.nextLine());
+				if (option == 1) {
+					return "ArrayList";
+				} else if (option == 2) {
+					return "LinkedList";
+				}
+			} catch (Exception e) {
+				continue;
+			}
+		}
+	}
+
+	private Integer[] fillArrayOfInt(int arraySize) {
+		Integer[] array = new Integer[arraySize];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.valueOf(i + 1);
+		}
+		return array;
+	}
+
+	// Разница заметна при 100000 элементах
 	private int askArraySize() {
 		while (true) {
 			drawTitle();
-			System.out.println(" Введите ");
+			printEmptyLines(14);
+			System.out.println(" Введите количество элементов массива больше нуля: ");
+			int count;
+			try {
+				Scanner in = new Scanner(System.in);
+				count = Integer.parseInt(in.nextLine());
+				if (count > 0 ) {
+					return count;
+				}
+			} catch (Exception e) {
+				continue;
+			}
 		}
-		return 0;
 	}
 
 	@Override
 	protected void drawTitle() {
 		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
-		 					" ║Задача №11. Выделить все различные слова. Для каждого слова подсчитать час- ║\r\n" + 
-		 					" ║   тоту его встречаемости. Учитывать регистр. Использовать класс HashMap    ║\r\n" +
+		 					" ║Задача №12. В кругу стоят N человек, пронумерованных от 1 до N. При ведении ║\r\n" +
+		 					" ║ счета по кругу вычеркивается каждый второй человек, пока не останется один ║\r\n" +
 							" ╚════════════════════════════════════════════════════════════════════════════╝");
 		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
-				 			" ║               Общая строка склеивается из строк файла task11.txt           ║\r\n" +
-				 			" ║       Различные слова с частотой будут записаны в файл task11output.txt    ║\r\n" +
+				 			" ║   Составить две программы, моделирующие процесс. Одна из программ должна   ║\r\n" +
+				 			" ║             использовать класс ArrayList, а вторая LinkedList              ║\r\n" +
 							" ╚════════════════════════════════════════════════════════════════════════════╝");
 	}
 
