c0198af192c40b217ae328a09f9f93f12afa55f0
supreme.testing.rus@gmail.com <supreme.testing.rus@gmail.com@a67e4f36-4e9d-a895-4f60-b2dded599e4f>
2014-11-09 10:47:59 +0000

    14/23 без 5

--- trunk/epamTask2/src/ru/epam/task2/collection/Graph.java
+++ trunk/epamTask2/src/ru/epam/task2/collection/Graph.java
@@ -5,34 +5,168 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import ru.epam.task2.gui.Task;
 
 public class Graph extends Task {
+	
+	private NumGraph graph;
 
 	public Graph(String shortName) {
 		super(shortName);
-		
 	}
 
 	@Override
 	protected void doLogic() {
-		// TODO Auto-generated method stub
-//		Collections.b
-//		Math.ceil(double)
+		int vertexCount = askVertexCount();
+		graph = new NumGraph(vertexCount);
+		while(true) {
+			drawSreen();
+			String option = askOption();
+			if (option.equals("+")) {
+				addEdgeToGraph();
+			} else if (option.equals("-")) {
+				removeEdgeFromGraph();
+			} else if (option.equals("0")) {
+				return;
+			}
+		}
+	}
+
+	private void removeEdgeFromGraph() {
+		while (true) {
+			drawSreen();
+			System.out.print(" Введите вершины удаляемого ребра (формат X,Y)");
+			try {
+				Scanner in = new Scanner(System.in);
+				String[] words = in.nextLine().split(",");
+				if (words.length != 2) {
+					continue;
+				}
+				int[] vertexes = new int[words.length];
+				for (int i = 0; i < words.length; i++) {
+					vertexes[i] = Integer.parseInt(words[i]);
+				}
+				if (!graph.removeEdge(vertexes[0], vertexes[1])){
+					printError(" Ошибка удаления ребра из графа!");
+				} else {
+					return;
+				}
+			} catch (Exception e) {
+				continue;
+			}
+		}
+		
+	}
+
+	private void drawSreen() {
+		drawTitle();
+		int stringsCount = drawgraph();
+		printEmptyLines(15 - stringsCount);
+	}
+	
+	private void addEdgeToGraph() {
+		while (true) {
+			drawSreen();
+			System.out.print(" Введите вершины добавляемого ребра (формат X,Y)");
+			try {
+				Scanner in = new Scanner(System.in);
+				String[] words = in.nextLine().split(",");
+				if (words.length != 2) {
+					continue;
+				}
+				int[] vertexes = new int[words.length];
+				for (int i = 0; i < words.length; i++) {
+					vertexes[i] = Integer.parseInt(words[i]);
+				}
+				if (!graph.addEdge(vertexes[0], vertexes[1])) {
+					printError(" Ошибка добавления ребра в граф!");
+				} else {
+					return;
+				}
+			} catch (Exception e) {
+				continue;
+			}
+		}
+		
+	}
+
+	private void printError(String string) {
+		drawSreen();
+		System.out.print(string + " ");
+		pressAnyKey();
+	}
+
+	private String askOption() {
+		System.out.print(" Введите действие с ребром (\"+\" добавление, \"-\" удаление, 0 выход в меню): ");
+		try {
+			Scanner in = new Scanner(System.in);
+			return in.nextLine();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private String[] splitStringEvery(String s, int interval) {
+	    int arrayLength = (int) Math.ceil(((s.length() / (double)interval)));
+	    String[] result = new String[arrayLength];
+	    if (result.length == 0) {
+	    	return result;
+	    }
+	    int j = 0;
+	    int lastIndex = result.length - 1;
+	    for (int i = 0; i < lastIndex; i++) {
+	        result[i] = s.substring(j, j + interval);
+	        j += interval;
+	    }
+	    result[lastIndex] = s.substring(j);
+	    return result;
+	}
+
+	private int askVertexCount() {
+		while (true) {
+			drawTitle();
+			printEmptyLines(15);
+			System.out.print(" Введите положительное количество вершин в графе: ");
+			int count;
+			try {
+				Scanner in = new Scanner(System.in);
+				count = Integer.parseInt(in.nextLine());
+				if (count > 0) {
+					return count;
+				}
+			} catch (Exception e) {
+				continue;
+			}
+		}
+	}
+
+	private int drawgraph() {
+		String[] strings = splitStringEvery(graph.toString(), 79);
+		for (String string : strings) {
+			System.out.println(string);
+		}
+		return strings.length;
 	}
 
 	@Override
 	protected void drawTitle() {
-		
-		
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+				 			" ║Задача №13. Реализовать класс - неориентированный граф. В конструкторе пере-║\r\n" + 
+				 			" ║ дать количество вершин. Реализовать методы быстрого добавления и удаления. ║\r\n" +
+				 			" ╚════════════════════════════════════════════════════════════════════════════╝");
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+				 			" ║ Добавление/удаление ребер происходит через запрос действия у пользователя: ║\r\n" +
+				 			" ║ \"+\" добавление, \"-\" удаление, 0 выход в меню. Результат выводится на экран ║\r\n" +
+				 			" ╚════════════════════════════════════════════════════════════════════════════╝");
 	}
 		
-	public class numGraph {
+	public class NumGraph {
 		
 		private final HashMap<Integer, List<Integer>> graphMap = new HashMap<Integer, List<Integer>>();
 		
-		public numGraph(int vertexCount) {
+		public NumGraph(int vertexCount) {
 			for (int i = 1; i <= vertexCount; i++) {
 				graphMap.put(Integer.valueOf(i), new ArrayList<Integer>());
 			}
@@ -50,13 +184,10 @@ public class Graph extends Task {
 	    public boolean addEdge(Integer vertex1, Integer vertex2) {
 	        if (vertex1.equals(vertex2)) return false;
 	        if (!hasVertex(vertex1) || !hasVertex(vertex2)) return false;
-	        if (hasEdge(vertex2, vertex1) || hasEdge(vertex1, vertex2)) return true;
+	        if (hasEdge(vertex2, vertex1) || hasEdge(vertex1, vertex2)) return false;
 	        List<Integer> edges1 = graphMap.get(vertex1);
-	        List<Integer> edges2 = graphMap.get(vertex2);
 	        edges1.add(vertex2);
-	        edges2.add(vertex1);
 	        Collections.sort(edges1);
-	        Collections.sort(edges2);
 	        return true;
 	    }
 	    
@@ -69,11 +200,8 @@ public class Graph extends Task {
 	        	vertex2 = temp;
 	        }
 	        List<Integer> edges1 = graphMap.get(vertex1);
-	        List<Integer> edges2 = graphMap.get(vertex2);
 	        edges1.remove(vertex2);
-	        edges2.remove(vertex1);
 	        Collections.sort(edges1);
-	        Collections.sort(edges2);
 	        return true;
 	    }
 	    
