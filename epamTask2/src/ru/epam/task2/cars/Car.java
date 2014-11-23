9d9a198f5d104f4754617b46f9f13b3d4743dbfc
supreme.testing.rus <supreme.testing.rus@gmail.com>
2014-11-23 20:23:08 +0000

    Осталось причесать 23ю (правильный титл и ввод К пользователем) + javadoc на все

--- epamTask2/src/ru/epam/task2/cars/Car.java
+++ epamTask2/src/ru/epam/task2/cars/Car.java
@@ -5,6 +5,9 @@ public class Car {
 	private final Integer startPos;
 	private final Integer velocity;
 	
+	private Integer startWay;
+	private Integer finWay;
+	
 	/**
 	 * @param startPos
 	 * @param velocity
@@ -13,6 +16,7 @@ public class Car {
 	public Car(Integer startPos, Integer velocity) {
 		this.startPos = startPos;
 		this.velocity = velocity;
+		this.startWay = startPos;
 	}
 
 	/**
@@ -28,6 +32,74 @@ public class Car {
 	public Integer getVelocity() {
 		return velocity;
 	}
+
+	/**
+	 * @return the startWay
+	 */
+	public Integer getStartWay() {
+		return startWay;
+	}
+
+	/**
+	 * @return the finWay
+	 */
+	public Integer getFinWay() {
+		return finWay;
+	}
+
+	/**
+	 * @param startWay the startWay to set
+	 */
+	public void setStartWay(Integer startWay) {
+		this.startWay = startWay;
+	}
+
+	/**
+	 * @param finWay the finWay to set
+	 */
+	public void setFinWay(Integer finWay) {
+		this.finWay = finWay;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((startPos == null) ? 0 : startPos.hashCode());
+		result = prime * result
+				+ ((velocity == null) ? 0 : velocity.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Car))
+			return false;
+		Car other = (Car) obj;
+		if (startPos == null) {
+			if (other.startPos != null)
+				return false;
+		} else if (!startPos.equals(other.startPos))
+			return false;
+		if (velocity == null) {
+			if (other.velocity != null)
+				return false;
+		} else if (!velocity.equals(other.velocity))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
--- epamTask2/src/ru/epam/task2/cars/FirstKOvertakings.java
+++ epamTask2/src/ru/epam/task2/cars/FirstKOvertakings.java
@@ -1,24 +1,70 @@
 package ru.epam.task2.cars;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.epam.task2.gui.Task;
 
 public class FirstKOvertakings extends Task{
+	
+	private OvertakingSet overs;
+	private List<Car> carList;
+	private final int K = 5;
 
 	public FirstKOvertakings(String shortName, String[] incomingStrings) {
 		super(shortName, incomingStrings);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void doLogic() {
-		// TODO Auto-generated method stub
-		
+		overs = new OvertakingSet();
+		drawTitle();
+		if (!setCarList()) {
+			System.out.println(" Ошибка чтения файла или неверный формат!\r\n" 
+							 + " Машины считаются построчно из файла task23.txt\r\n" 
+							 + " в формате СтартПозиция Скорость");
+			printEmptyLines(12);
+			return;
+		}
+		overs.setAllOvertakings(carList);
+		if (K > overs.getOverCount()) {
+			System.out.println(" Число К больше реального числа обгонов");
+			printEmptyLines(14);
+			return;
+		}
+		overs = new OvertakingSet();
+		overs.setKfirstOvertakings(carList, K);
+		System.out.println(" Количество обгонов равно: " + overs.getOverCount());
+		writeStringsToFile("task22output.txt", overs.getStrings());
+		printEmptyLines(14);
 	}
 
 	@Override
 	protected void drawTitle() {
-		// TODO Auto-generated method stub
-		
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+				 			" ║ Задача №22. На прямой гоночной трассе стоит N автомобилей, для каждого из  ║\r\n" +
+				 			" ║которых известны начальное положение и скорость. Cколько произойдет обгонов?║\r\n" +
+				 			" ╚════════════════════════════════════════════════════════════════════════════╝");
+		 System.out.println(" ╔════════════════════════════════════════════════════════════════════════════╗\r\n" +
+				 			" ║                 Количество обгонов выводится численно на экран             ║\r\n" +
+				 			" ║      Описание обгонов построчно выгружается в файл task22output.txt        ║\r\n" +
+							" ╚════════════════════════════════════════════════════════════════════════════╝");
 	}
 
+	private boolean setCarList() {
+		carList = new ArrayList<Car>();
+		String[] cars = getStrings();
+		if (cars.length < 1) return false;
+		try {
+			for (String car : cars) {
+				String[] carData = car.split("\\s");
+				if (carData.length != 2) return false;
+				carList.add(new Car(Integer.parseInt(carData[0]), Integer.parseInt(carData[1])));
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+		
 }
