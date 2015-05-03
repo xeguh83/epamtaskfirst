87433a3cdc9d830db4270905ffd07dee095845bc
supreme.testing.rus <supreme.testing.rus@gmail.com>
2015-05-03 21:51:52 +0000

    Рефакторинг

--- KursBase/src/ru/etu/oop/data/DBAdapter.java
+++ KursBase/src/ru/etu/oop/data/DBAdapter.java
@@ -14,60 +14,15 @@ public class DBAdapter {
 	public static final String BASE_PASSWORD = "12345678";
 	public static final String BASE_URL_FOR_JDBC_DRIVER = "jdbc:postgresql://localhost:5432/kurs";
 	
-	public static void tryConnection() {
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection(BASE_URL_FOR_JDBC_DRIVER, BASE_USER, BASE_PASSWORD);
-			System.out.println("Соединение установлено!");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (con != null) {
-				con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public static void showRooms() {
-		Connection con = null;
-		Statement st = null;
-		ResultSet rs = null;
-		try {
-			con = DriverManager.getConnection(BASE_URL_FOR_JDBC_DRIVER, BASE_USER, BASE_PASSWORD);
-			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM rooms");
-			while (rs.next()) {
-				System.out.println(rs.getInt(1) + " " + rs.getInt(2) + " " + rs.getString(3));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (con != null) {
-				con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
+
 	public static List<Room> getRooms() {
-		List<Room> roomList = null;
+		List<Room> roomList = new ArrayList<>();
 		Connection con = null;
-		Statement st = null;
 		ResultSet rs = null;
 		try {
-			con = DriverManager.getConnection(BASE_URL_FOR_JDBC_DRIVER, BASE_USER, BASE_PASSWORD);
-			roomList = new ArrayList<>();
-			st = con.createStatement();
-			rs = st.executeQuery("SELECT * FROM rooms");
+			rs = connectAndQuery(con);
 			while (rs.next()) {
-				roomList.add(new Room(Integer.toString(rs.getInt(1)), Integer.toString(rs.getInt(2)), rs.getString(3)));
+				roomList.add(createRoom(rs));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -83,6 +38,19 @@ public class DBAdapter {
 		
 		return roomList;
 		
+	}
+
+
+	private static Room createRoom(ResultSet rs) throws SQLException {
+		return new Room(Integer.toString(rs.getInt(1)), Integer.toString(rs.getInt(2)), rs.getString(3));
+	}
+
+
+	private static ResultSet connectAndQuery(Connection con) throws SQLException {
+		Statement st = null;
+		con = DriverManager.getConnection(BASE_URL_FOR_JDBC_DRIVER, BASE_USER, BASE_PASSWORD);
+		st = con.createStatement();
+		return st.executeQuery("SELECT * FROM rooms");
 	}
 
 }
